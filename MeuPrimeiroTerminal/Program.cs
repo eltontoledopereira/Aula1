05d8ce54a5e6cafe10e3cae424930223f5a89ef9
carolbett <cacabett@gmail.com>
2019-08-17 11:37:26 -0300

    manipulando strings

---
+++ MeuPrimeiroTerminal/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MeuPrimeiroTerminal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string txt1 = "Hello World!";
+            string txt2 = "Elton, com idade de:";
+            int idade = 36;
+            string txt3 = $"{txt1} {txt2} {idade}";
+
+            Console.WriteLine(txt3);
+        }
+    }
+}
